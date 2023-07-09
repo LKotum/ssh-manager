727279705ffc95c9dcc3ecd7db87e59f7876c8db
LKotum <KrutadNemo@yandex.ru>
2023-07-09 23:26:34 +0300

    _dupenv_s edit

--- libs/hostspsr/include/hostspsr.h
+++ libs/hostspsr/include/hostspsr.h
@@ -33,7 +33,8 @@ class HostsParser
 			std::string _hosts_path = "\\.ssh\\hosts.ini";
 			size_t size;
 			char *path;
-			std::string _Path = _dupenv_s(&path, &size, "USERPROFILE") + _hosts_path;
+			_dupenv_s(&path, &size, "USERPROFILE")
+			std::string _Path = path + _hosts_path;
 		#else
 			std::string _hosts_path = "/.ssh/hosts.ini";
 			std::string _Path = getenv("HOME") + _hosts_path;
