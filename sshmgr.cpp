74f9684eb45f6971f1526d509b0a5029a4e14553
BadWolf <115115085+BadWolf1st@users.noreply.github.com>
2023-07-16 17:43:31 +0300

    UPDATE 1.2.0 Added new function: Cyclic connection to the host, which allows you to connect to the host multiple times with an interruption of 10 connection attempts. For example, if you reboot your remote machine, then you will not need to re-enter the command to connect.

--- sshmgr.cpp
+++ sshmgr.cpp
@@ -18,7 +18,7 @@ const char *logo =
 	"\033[33m| \\____) |\033[32m| \\____) |\033[34m _| |  | |_ \033[0m\n"
 	"\033[33m \\______.'\033[32m \\______.'\033[34m|____||____|\033[0m\n"
 	"\n"
-	"SSH manager (\033[31mv1.1.1\033[0m)\n"
+	"SSH manager (\033[31mv1.2.0-BETA\033[0m)\n"
 	"By \033[35mMaxLane\033[0m and \033[35mBadWolf\033[0m\n"
 	"\n"
 	"Feedback:\n"
@@ -31,6 +31,7 @@ struct sshmngr
 	bool version{false};
 	bool newconnect{false};
 	bool connect{false};
+	bool reconnect{false};
 	bool remove{false};
 	bool list{false};
 	bool main{true};
@@ -89,6 +90,8 @@ const unordered_map<string, OneArgHandle> OneArgs
 	{"--connect", [](sshmngr &s, const string &arg){ s.connect = check(s, arg); s.main = false; }},
 	{"-r", [](sshmngr &s, const string &arg){ s.remove = check(s, arg); s.main = false; }},
 	{"--remove", [](sshmngr &s, const string &arg){ s.remove = check(s, arg); s.main = false; }},
+	{"-cl", [](sshmngr& s, const string& arg) {s.connect = check(s, arg); s.main = false; s.reconnect = true; }},
+	{"--connect-looping", [](sshmngr& s, const string& arg) {s.connect = check(s, arg); s.main = false; s.reconnect = true; }},
 };
 
 // bool doctor()
@@ -132,24 +135,26 @@ string request(){
 void printFlagsHelp()
 {
 	const char *Help=
-	"\033[33m-h\033[0m,  \033[33m--help\033[0m                                Get this help\n"
-	"\033[33m-v\033[0m,  \033[33m--version\033[0m                             Get information about program\n"
-	"\033[33m-l\033[0m,  \033[33m--list\033[0m                                Get list of hosts\n"
-	"\033[33m-n\033[0m,  \033[33m--new\033[0m                                 Used to save new connections\n"
-	"\033[33m-nc\033[0m, \033[33m--new-connect\033[0m                         To save and connect quickly\n"
-	"\033[33m-c\033[0m,  \033[33m--connect\033[0m + \033[35m(argument (digital))\033[0m      Used for connect to host\n"
-	"\033[33m-r\033[0m,  \033[33m--remove\033[0m + \033[35m(argument (digital))\033[0m       Used to remove host preset\n";
+	"\033[33m-h\033[0m,  \033[33m--help\033[0m                                    Get this help\n"
+	"\033[33m-v\033[0m,  \033[33m--version\033[0m                                 Get information about program\n"
+	"\033[33m-l\033[0m,  \033[33m--list\033[0m                                    Get list of hosts\n"
+	"\033[33m-n\033[0m,  \033[33m--new\033[0m                                     Used to save new connections\n"
+	"\033[33m-nc\033[0m, \033[33m--new-connect\033[0m                             To save and connect quickly\n"
+	"\033[33m-c\033[0m,  \033[33m--connect\033[0m + \033[35m(argument (digital))\033[0m          Used for connect to host\n"
+	"\033[33m-r\033[0m,  \033[33m--remove\033[0m + \033[35m(argument (digital))\033[0m           Used to remove host preset\n"
+	"\033[33m-cl\033[0m,  \033[33m--connect-looping\033[0m + \033[35m(argument (digital))\033[0m It is used in multiple cyclic connection to the host\n";
 	cout << Help << endl;
 };
 
 void printHelp(){
 	const char *Help=
-	"\033[33mh\033[0m, \033[33mhelp\033[0m          Get this help\n"
-	"\033[33ml\033[0m, \033[33mlist\033[0m          Get saved hosts templates\n"
-	"\033[33mn\033[0m, \033[33mnew\033[0m           Used to create new connections\n"
-	"\033[33mc\033[0m, \033[33mconnect\033[0m       Used for connect to host\n"
-	"\033[33mr\033[0m, \033[33mremove\033[0m        Used to remove host preset\n"
-	"\033[33mq\033[0m, \033[33mquit\033[0m, \033[33mexit\033[0m    Used to terminate the program\n";
+	"\033[33mh\033[0m, \033[33mhelp\033[0m             Get this help\n"
+	"\033[33ml\033[0m, \033[33mlist\033[0m             Get saved hosts templates\n"
+	"\033[33mn\033[0m, \033[33mnew\033[0m              Used to create new connections\n"
+	"\033[33mc\033[0m, \033[33mconnect\033[0m          Used for connect to host\n"
+	"\033[33mr\033[0m, \033[33mremove\033[0m           Used to remove host preset\n"
+	"\033[33mq\033[0m, \033[33mquit\033[0m, \033[33mexit\033[0m       Used to terminate the program\n"
+	"\033[33mcl\033[0m, \033[33mconnect looping\033[0m It is used in multiple cyclic connection to the host\n";
 	cout << Help << endl;
 };
 
@@ -172,13 +177,12 @@ void addHost(HostsParser &hosts, bool connect = false)
 	cin >> answer;
 	if (answer == "y"){
 		system(command(user, host, port).c_str());
-
 	}
 	else if (answer == "n"){
 		return;
 	}
 	else {
-		command(user, host, port);
+		system(command(user, host, port).c_str());
 	}
 };
 
@@ -212,9 +216,26 @@ void removeHost(HostsParser &hosts, int index)
 	}
 };
 
-void connectHost(map<string, string> &host)
+void connectHost(map<string, string> &host, bool reconect = false)
 {
-	system(command(host["user"], host["host"], host["port"]).c_str());
+	if (!reconect) {
+		system(command(host["user"], host["host"], host["port"]).c_str());
+		return;
+	}
+	int counter = 0;
+	do
+	{
+		system(command(host["user"], host["host"], host["port"]).c_str());
+		counter++;
+		if (counter == 10) {
+			cout << "\033[32m[INFO]\033[0m Continue reconnecting? \033[33m(\033[4my\033[0m\033[33m/n)\033[0m: ";
+			string answer;
+			cin >> answer;
+			if (answer == "n") {
+				reconect = false;
+			}
+		}
+	} while (reconect);
 };
 
 int commander(string comm){
@@ -236,6 +257,9 @@ int commander(string comm){
 	// 7 is path
 	else if ((comm == "quit") || (comm == "q") || (comm == "exit") || (comm == "quit()")){
 		return 8;
+	}
+	else if ((comm == "connect looping") || (comm == "cl")) {
+		return 9;
 	}
 
 	return 1;
@@ -272,7 +296,7 @@ int main(int argc, char* argv[])
 	if((args.connect) && (!args.newconnect)){
 		if (hosts.hostExicst(args.host.value())){
 			map<string, string> host = hosts.getHost(args.host.value());
-			connectHost(host);
+			connectHost(host, args.reconnect);
 		}
 		else{
 			cout << "\033[31m[WARN]\033[0m Could not find this host!\n";
@@ -311,33 +335,33 @@ int main(int argc, char* argv[])
 				cout << "\033[31m[WARN]\033[0m Wrong command! Type \033[33m[help]\033[0m to see all commands\n";
 				break;
 			case 2:
-				{
-					cout << "\033[32m[INFO]\033[0m Enter the number of host: ";
-					cin >> answer;
-					int q;
-					if(check(answer, q)){
-						if (hosts.hostExicst(q)){
-							map<string, string> host = hosts.getHost(q);
-							connectHost(host);
-						}
-						else{
-							cout << "\033[31m[WARN]\033[0m Could not find this host!\n";
-						}
+			{
+				cout << "\033[32m[INFO]\033[0m Enter the number of host: ";
+				cin >> answer;
+				int q;
+				if(check(answer, q)){
+					if (hosts.hostExicst(q)){
+						map<string, string> host = hosts.getHost(q);
+						connectHost(host);
+					}
+					else{
+						cout << "\033[31m[WARN]\033[0m Could not find this host!\n";
 					}
 				}
 				break;
+			}
 			case 3:
 				addHost(hosts);
 				break;
 			case 4:
-				{
-					cout << "\033[32m[INFO]\033[0m Enter the number of host: ";
-					cin >> answer;
-					int q;
-					if(check(answer, q))
-						removeHost(hosts, q);
-				}
+			{
+				cout << "\033[32m[INFO]\033[0m Enter the number of host: ";
+				cin >> answer;
+				int q;
+				if(check(answer, q))
+					removeHost(hosts, q);
 				break;
+			}
 			case 5:
 				printHelp();
 				break;
@@ -350,6 +374,22 @@ int main(int argc, char* argv[])
 			case 8:
 				return 0;
 				break;
+			case 9:
+			{
+				cout << "\033[32m[INFO]\033[0m Enter the number of host: ";
+				cin >> answer;
+				int q;
+				if (check(answer, q)) {
+					if (hosts.hostExicst(q)) {
+						map<string, string> host = hosts.getHost(q);
+						connectHost(host, true);
+					}
+					else {
+						cout << "\033[31m[WARN]\033[0m Could not find this host!\n";
+					}
+				}
+				break;
+			}
 			default:
 				cout << "[WARN] Wrong argument! Type [help] to see all commands\n";
 				break;
