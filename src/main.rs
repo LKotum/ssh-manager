1537630affaa6073e9e611931de86081cbc0eaa4
LKotum <88040466+LKotum@users.noreply.github.com>
2024-05-12 22:38:49 +0300

    Begining...
    Co-authored-by: Raskutin Sergey <BadWolf1st@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::process::{Command, Stdio};
+
+struct SSHConnect;
+
+impl SSHConnect {
+    fn connect(username: &str, hostname: &str, port: &str) {
+        let ssh_command = format!("ssh -p {} {}@{}", port, username, hostname);
+
+        match Command::new("sh")
+            .arg("-c")
+            .arg(&ssh_command)
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()
+        {
+            Ok(status) => {
+                if !status.success() {
+                    eprintln!("Failed to execute SSH command: {}", status);
+                }
+            }
+            Err(err) => {
+                eprintln!("Failed to execute SSH command: {}", err);
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let username = "";
+    let hostname = "";
+    let port: &str = "";
+
+    SSHConnect::connect(username, hostname, port);
+    println!("Connection closed!")
 }
